5c3f1adea3e31af51b87a8dc97aa4a71ee7d9b75
Suryansh Panwar <panwarsuryansh2@gmail.com>
2022-05-01 16:58:11 +0530

    databse added

--- node.js
+++ node.js
@@ -1,21 +1,40 @@
 const express = require(`express`);
+const mongoose = require('mongoose');
 const bodyparser = require(`body-parser`);
 const ejs = require(`ejs`);
 const _=require(`lodash`);
 const { constant } = require("async");
-const homecontent = `Lorem ipsum, dolor sit amet consectetur adipisicing elit.homecontent Excepturi rem ipsum, dolor sit amet consectetur adipisicing elit.homecontent Excepturi ducimus placeat doloducimus placeat dolores, sequi totam mollitia! Dicta veritatis, deleniti ullam voluptatum laudantium, esse ratione fugit quae natus modi amet neque facere explicabo ipsum veniam libero sit officiis delectus repellat nesciunt rerum! Quidem eaque quasi, ullam et iste laboriosam blanditiis tempore sequi!5`;
+
+ //databse connection
+const uri = "mongodb+srv://admin-suryansh:suryanshpanwar@cluster0.snk5b.mongodb.net/newsletter?retryWrites=true&w=majority";
+
+mongoose.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true})
+.then(() => console.log(`mongoose connected`))
+.catch(err => console.error(err))
+
+//data base schema 
+const newsSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    post: {type: String, required: true}
+})
+
+const User = mongoose.model('User', newsSchema)
+//created database schema
+
+let news = [];
+
+// const homecontent = `Lorem ipsum, dolor sit amet consectetur adipisicing elit.homecontent Excepturi rem ipsum, dolor sit amet consectetur adipisicing elit.homecontent Excepturi ducimus placeat doloducimus placeat dolores, sequi totam mollitia! Dicta veritatis, deleniti ullam voluptatum laudantium, esse ratione fugit quae natus modi amet neque facere explicabo ipsum veniam libero sit officiis delectus repellat nesciunt rerum! Quidem eaque quasi, ullam et iste laboriosam blanditiis tempore sequi!5`;
 
 const aboutcontent=`Hey there ! welcome to this beautiful blog website where you can share your views freely without registering . this website is 
 made and designed by Suryansh Panwar using EJS , node , css . hope you like this webiste , many more changes will come in this webiste in future updates
 so for that stay tunned stay connected`;
 const contactcontant=`contact me through Email panwarsuryansh2gmail.com`;
 
-let composearr =[];
 let key;
 
 const app = express();
 
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(`public`));
 
 app.set(`view engine`, `ejs`);
@@ -23,10 +42,11 @@ app.set(`view engine`, `ejs`);
 app.listen(8000, function () {
     console.log(`listning at port 8000`)
 })
-app.get(`/`, (req, res) => {
-    res.render("Home",{homecont:homecontent,
-                       arr:composearr                  
-    });
+app.get(`/`,async (req, res) => {
+    
+   const all = await User.find().then(res => news = res).catch(err => console.log(err));
+    // console.log(all);   
+    res.render("Home", {"news": all});
 })
 app.get(`/about`, (req, res) => {
     res.render("about",{homecont:aboutcontent});
@@ -44,8 +64,19 @@ app.post(`/compose`,(req,res)=>{
       title:req.body.messagetitle,
       message:req.body.message
     }
+    
+    
+
+
+    // news.push(compose);
+
+    //saving the news in databse online
+
+    let new_news = new User({name:req.body.messagetitle , post:req.body.message});
+    new_news.save().then(()=> res.status(201).json(new_news)).catch(err => res.status(400).send(err))
    
-    composearr.push(compose);
+
+    //news saved in DB
 
     res.redirect(`/`);
 
@@ -69,3 +100,19 @@ app.get(`/posts/:postname`, (req,res)=>{
 
 })
 
+app.patch("/", (req, res) => {
+    let compose ={
+        title:req.body.messagetitle,
+        message:req.body.message
+      }
+ 
+      User.find({title}).updateOne({compose})
+})
+
+app.delete("/", (req, res) => {
+    let id = req.body.id;
+    let post = User.findById(id)
+
+    if (post) post.deleteOne()
+    else res.send("deleted")
+})
